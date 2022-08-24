e3ab9892fb005a7471ae2bf6f5095eca55f349f3
leehojin <yui8870@gmail.com>
2022-08-25 01:22:33 +0900

    20220825

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>portfolio - 이호진 디자이너</title>
+        <title>portfolio - 이호진 웹디자이너</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="Free Website Template" name="keywords">
         <meta content="Free Website Template" name="description">
