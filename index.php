dc263506ee902e31eb4b1561fdc557de5148e577
leehojin <yui8870@gmail.com>
2022-11-17 02:24:50 +0900

    20221117224

--- index.php
+++ index.php
@@ -521,25 +521,6 @@
             </div>
         </div>
 
-
-
-        
-        <!-- Banner Start -->
-        <div class="banner wow zoomIn" data-wow-delay="0.1s">
-            <div class="container">
-                <div class="section-header text-center">
-                    <p>Awesome Discount</p>
-                    <h2>Get <span>30%</span> Discount</h2>
-                </div>
-                <div class="container banner-text">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulputate. Aliquam metus tortor, auctor id gravida condimentum, viverra quis sem. Curabitur non nisl nec nisi scelerisque maximus. 
-                    </p>
-                    <a class="btn">Order Nowdd</a>
-                </div>
-            </div>
-        </div>
-        <!-- Banner End -->
         
           <!-- Blog Start -->
           <div class="blog" id="blog">
